b6beab51ef75304f301d1fa0e249ebff8cce419d
vkarthik22 <vkpower02@gmail.com>
2025-06-30 16:37:07 -0500

    feat: Added a POST endpoint to put send all chat message inputs from user

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello from your Node.js Backend!')
+})
+
+app.post('/chat', (req, res) => {
+  const userMsg = req.body.message
+  console.log('This is the user  message: ', userMsg)
+  const geminiMsg = `Message recieved: $"{userMsg}"`
+  res.json({ reply: geminiMsg })
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`)
+})
